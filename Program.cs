dfe7b6c784e15f32440cd34ef0b950997ea47e99
Пользователь <@-PC>
2023-05-13 15:16:12 +0200

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿// ДЗ цифра
+Console.Write("Введите радиус ");
+double r = double.Parse(Console.ReadLine());
+const double pi = Math.PI;
+double S = pi * r * r;
+double L = 2 * pi * r;
+Console.WriteLine($"Площадь окружности = {S:f2}");
+Console.WriteLine($"Длина окружности = {L:f2}");
